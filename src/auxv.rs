4cb250b81ec45313886959bcc6523781b8b5222b
Eric Lagergren <eric@ericlagergren.com>
2024-04-22 00:26:22 -0700

    x
    Signed-off-by: Eric Lagergren <eric@ericlagergren.com>

--- src/auxv.rs
+++ src/auxv.rs
@@ -529,13 +529,39 @@ mod rt {
     /// Returns a pointer to the auxiliary vector.
     pub fn auxv() -> *const AuxVal {
         unsafe {
+            use core::ffi::{c_char, c_int, c_ulong};
+
+            #[repr(C)]
+            struct RScopeElem {
+                _r_list: *mut *mut (),
+                _r_nlist: c_int,
+            }
+            #[repr(C)]
+            struct RtldGlobal {
+                _dl_debug_mask: c_int,
+                _dl_platform: *const c_char,
+                _dl_platformlen: usize,
+                _dl_pagesize: usize,
+                _dl_minsigstacksize: usize,
+                _dl_inhibit_cache: c_int,
+                _dl_initial_searchlist: RScopeElem,
+                _dl_clktck: c_int,
+                _dl_verbose: c_int,
+                _dl_debug_fd: c_int,
+                _dl_lazy: c_int,
+                _dl_bind_not: c_int,
+                _dl_dynamic_weak: c_int,
+                _dl_fpu_control: c_ulong,
+                _dl_hwcap: u64,
+                _dl_auxv: *const AuxVal,
+            }
             extern "C" {
-                static _rtld_global_ro: *const ();
+                static _rtld_global_ro: *const RtldGlobal;
                 //static _dl_auxv: *const AuxVal;
             }
-            // if !_dl_auxv.is_null() {
-            //     return _dl_auxv;
-            // }
+            if !_dl_auxv.is_null() {
+                return _rtld_global_ro._dl_auxv;
+            }
         }
         let mut ptr = AUXV.load(Ordering::Relaxed);
         if ptr.is_null() {
