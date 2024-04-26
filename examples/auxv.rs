bd5c9d41a93a1e0e1178e7bb8a62b41887ff713e
Eric Lagergren <eric@ericlagergren.com>
2024-04-25 21:07:54 -0700

    x
    Signed-off-by: Eric Lagergren <eric@ericlagergren.com>

--- examples/auxv.rs
+++ examples/auxv.rs
@@ -11,11 +11,51 @@
 ))]
 
 use core::{
+    arch::asm,
     ffi::{c_char, c_int, c_void},
     fmt::{self, Write},
 };
 
 use sys_auxv::AuxVec;
+
+/*
+TEXT	·Syscall(SB),NOSPLIT,$0-56
+    CALL	runtime·entersyscall<ABIInternal>(SB)
+    MOVQ	trap+0(FP), AX	// syscall entry
+    MOVQ	a1+8(FP), DI
+    MOVQ	a2+16(FP), SI
+    MOVQ	a3+24(FP), DX
+    SYSCALL
+    JCC	ok
+    MOVQ	$-1, r1+32(FP)	// r1
+    MOVQ	$0, r2+40(FP)	// r2
+    MOVQ	AX, err+48(FP)	// errno
+    CALL	runtime·exitsyscall<ABIInternal>(SB)
+    RET
+ok:
+    MOVQ	AX, r1+32(FP)	// r1
+    MOVQ	DX, r2+40(FP)	// r2
+    MOVQ	$0, err+48(FP)	// errno
+    CALL	runtime·exitsyscall<ABIInternal>(SB)
+    RET
+*/
+unsafe fn syscall(trap: i64, a1: i64, a2: i64, a3: i64) -> (i64, i64) {
+    let r1;
+    let r2;
+    let err;
+    asm!(
+        "syscall",
+        "setc dl",
+        "movzx dl, {err}"
+        inlateout("rax") trap => r1,
+        in("rdi") a1,
+        in("rsi") a2,
+        inlateout("rdx") a3 => r2,
+        out(reg) err,
+        options(nostack),
+    );
+    (r1, r2)
+}
 
 #[no_mangle]
 unsafe extern "C" fn memcpy(dst: *mut c_void, src: *const c_void, n: usize) -> *mut c_void {
@@ -33,6 +73,14 @@ unsafe extern "C" fn memset(dst: *mut c_void, c: c_int, len: usize) -> *mut c_vo
     }
     dst
 }
+
+#[no_mangle]
+unsafe extern "C" fn atexit(_function: Option<extern "C" fn()>) -> c_int {
+    0
+}
+
+#[no_mangle]
+unsafe extern "C" fn exit(_status: c_int) {}
 
 #[no_mangle]
 pub extern "C" fn main(_argc: c_int, _argv: *const *const c_char) -> c_int {
